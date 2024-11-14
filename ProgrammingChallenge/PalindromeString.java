8c4f9d229678df7f4784cdc9d2268ae722586502
marcoyimyaem <marcoyimyaem@gmail.com>
2024-11-14 03:54:12 +0000

    day 19

--- ProgrammingChallenge/PalindromeString.java
+++ ProgrammingChallenge/PalindromeString.java
@@ -1,6 +1,20 @@
 package ProgrammingChallenge;
 
+    
 public class PalindromeString {
+    public String checkPalindrome(String word){
+        String revString = "";
+        for(int y = word.length()-1;y>=0;y--){
+            revString+=word.charAt(y);
+
+        }
+        System.out.println(revString);
+        return (revString.equals(word))?"Palindrome":"not Palindrome"; 
+    }
+    public static void main(String[] args) {
+        PalindromeString ps = new PalindromeString();
+        System.out.println(ps.checkPalindrome("level"));
+    }
     //level
     //l
     //le
--- Tester.java
+++ Tester.java
@@ -1,5 +1,21 @@
-import 
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ProgrammingChallenge.PalindromeString;
 public class Tester {
     @Test
+    void PalindromerValid(){
+        PalindromeString ps = new PalindromeString();
+        String ss =ps.checkPalindrome("level");
+        assertEquals("Palindrome",ss,"invalid");
+    }
+    @Test
+    void PalindromerInvalid(){
+        PalindromeString ps = new PalindromeString();
+        String ss =ps.checkPalindrome("levels");
+        assertEquals("not Palindrome",ss,"invalid");
+    }
+    @Test
+    void addTwonumbers(){
+        assertEquals(5,(4+1),"incorrect computation");
+    }
 }
